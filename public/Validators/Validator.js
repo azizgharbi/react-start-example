34bdf9d94e9178b7cfedf02850a17888560e7c18
aziz gharbi <agharbi@inovva.com>
2018-05-17 16:46:42 -0400

    isvalidate function add option

--- public/Validators/Validator.js
+++ public/Validators/Validator.js
@@ -5,23 +5,31 @@ import {
 
 var Validator = {
 
-    isValidate: (value, type) => {
+    isValidate: (value, type, required = false) => {
         if (value) {
 
             switch (type) {
                 case "String":
                     if (!isNaN(value)) {
                         throw Error("String Required");
+                    } else {
+                        return value;
                     }
                     break;
                 case "Number":
                     if (isNaN(value)) {
                         throw Error("Number Required");
+                    } else {
+                        return value;
                     }
                     break;
             }
         } else {
-            throw Error("this filed is required");
+            if (required) {
+                throw Error("this filed is required");
+            } else {
+                return value;
+            }
         }
     },
 
--- public/components/Main.jsx
+++ public/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "./Title"
 import List from "./Liste"
-import {Validator} from "./../Validators/Validator";
+import { Validator } from "./../Validators/Validator";
 
 
 
@@ -9,81 +9,86 @@ export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      liste :  [
-      {name:"aziz",age : 15},
-      {name:"alex",age : 20},
-      {name:"dave",age : 18},
-      {name:"ana",age : 65}
-    ],
+      liste: [
+        { name: "aziz", age: 15 },
+        { name: "alex", age: 20 },
+        { name: "dave", age: 18 },
+        { name: "ana", age: 65 }
+      ],
       nameValue: "",
-      ageValue:0,
-      errorMessage:{ErrorName : "" , errorAge : ""}
+      ageValue: "",
+      errorMessage: { ErrorName: "", errorAge: "" }
     }
   }
-    
+
   handleClick() {
     console.log("hello");
   }
 
+  // just add set required to true if it's required
+
   handleChangeName(event) {
-    
+
     try {
-      this.setState({nameValue:  Validator.isValidate(event.target.value,"String")});      
+      this.setState({ nameValue: Validator.isValidate(event.target.value, "String") });
     } catch (error) {
       this.setState({
-        errorMessage: {ErrorName : error.message , errorAge : ""}
+        errorMessage: { ErrorName: error.message, errorAge: "" }
       });
     }
+
   }
 
   handleChangeAge(event) {
-    try{
-    this.setState({ageValue: Validator.isValidate(event.target.value,"Number")});
+
+    try {
+      this.setState({ ageValue: Validator.isValidate(event.target.value, "Number") });
     } catch (error) {
       this.setState({
-        errorMessage: {ErrorName : "" , errorAge : error.message}
+        errorMessage: { ErrorName: "", errorAge: error.message }
       });
     }
+
   }
 
- 
-  onSubmit(event){
+
+  onSubmit(event) {
     event.preventDefault();
-        let humanBeing = {
-            name :this.state.nameValue, 
-            age : this.state.ageValue
-        };
+    let humanBeing = {
+      name: this.state.nameValue,
+      age: this.state.ageValue
+    };
 
-        this.setState({
-            nameValue: '',
-            ageValue: '',
-            errorMessage: '',
-            liste: [...this.state.liste, humanBeing]
-        });
+    this.setState({
+      nameValue: '',
+      ageValue: '',
+      errorMessage: '',
+      liste: [...this.state.liste, humanBeing]
+    });
   }
 
-    render() {
-        
-      return (
-        <div>
-            <Title title = "this a prop title" description="Lorem ipsum dolor sit amet, consectetur" />
-            <List items={this.state.liste}/>
-            <form onSubmit={this.onSubmit.bind(this)}>
-                <label>
-                  Name:
+  render() {
+
+    return (
+      <div>
+        <Title title="this a prop title" description="Lorem ipsum dolor sit amet, consectetur" />
+        <List items={this.state.liste} />
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>
+            Name:
                   <input type="text" value={this.state.nameValue} onChange={this.handleChangeName.bind(this)} />
-                  <p>{this.state.errorMessage.ErrorName}</p>
-                </label>
-                <label>
-                  Age:
-                  <input type="text" value={this.state.ageValue} onChange={this.handleChangeAge.bind(this)} />
-                  <p>{this.state.errorMessage.errorAge}</p>
-                </label>
-                  <input type="submit" value="Submit" />
-            </form>
-            <button onClick={this.handleClick.bind(this)}> click Me !</button>
-        </div>
-        
-      );
-    }
+            <p>{this.state.errorMessage.ErrorName}</p>
+          </label>
+          <label>
+            Age:
+                  <input type="number" value={this.state.ageValue} onChange={this.handleChangeAge.bind(this)} />
+            <p>{this.state.errorMessage.errorAge}</p>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <button onClick={this.handleClick.bind(this)}> click Me !</button>
+      </div>
+
+    );
+  }
 }
